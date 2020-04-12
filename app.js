8b629cbd26d7cede28125cc92863431c20f108ee
rguichette <rguichette@ralphgui@gmail.com>
2020-04-11 21:58:51 -0400

    init application

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+return 	res.send("done")
+});
+
+
+
+
+
+
+
+
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Server started on 3000`);
+});
